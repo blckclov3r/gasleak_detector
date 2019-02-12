54a0972bcd2e6c7cf23fa770c6b869d6caced639
blckclov3r <blckclov3r@gmail.com>
2019-02-11 21:33:45 -0800

    saved

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+ Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1000);
 }
