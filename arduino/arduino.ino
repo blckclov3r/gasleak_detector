73a053bea627c82c8fbddfff4e1c8341d6953c0b
blckclov3r <blckclov3r@gmail.com>
2019-02-11 22:31:30 -0800

    saved

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,58 @@
+//void setup() {
+//  // put your setup code here, to run once:
+//  Serial.begin(9600);
+//}
+//
+//void loop() {
+// 
+//  
+//  int sensorValue = analogRead(A0);
+//  Serial.print("Sensor state: ");
+//  Serial.println(sensorValue);
+//  delay(1000);
+//
+//  
+//}
+
+#include <SoftwareSerial.h>
+
+// Configure software serial port
+SoftwareSerial SIM900(7, 8); 
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
+  // Make sure that corresponds to the baud rate of your module
+  SIM900.begin(19200);
+  // Give time to your GSM shield log on to network
+  delay(20000);   
+  
+  // Send the SMS
+  sendSMS();
 }
 
-void loop() {
- 
-  
-  int sensorValue = analogRead(A0);
-  Serial.println("Sensor State: "+sensorValue);
+void loop() { 
+  sendSMS();
   delay(1000);
+}
 
+void sendSMS() {
+  // AT command to set SIM900 to SMS mode
+  SIM900.print("AT+CMGF=1\r"); 
+  delay(100);
+
+  // REPLACE THE X's WITH THE RECIPIENT'S MOBILE NUMBER
+  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
+  SIM900.println("AT + CMGS = \"+639555076271\""); 
+  delay(100);
   
+  // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
+  SIM900.println("Message example from JOhnpaul guinitaran."); 
+  delay(100);
+
+  // End AT command with a ^Z, ASCII code 26
+  SIM900.println((char)26); 
+  delay(100);
+  SIM900.println();
+  // Give module time to send SMS
+  delay(5000); 
 }
